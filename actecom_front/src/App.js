162705b5816529249fe0b5db51a1961e882bd77e
Valhalla <mr.miquel@hotmail.com>
2023-05-17 13:08:07 +0200

    Se instala dependencia para uso de logos react-logos Se empieza el diseño del Navbar

--- actecom_front/src/App.js
+++ actecom_front/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './components/Logo.jsx';
 
 function App() {
   return (
-    <div className="construccion">
-      <h1>ACTECOM</h1>
-      <h3>- Página web en creación -</h3>
-      <p>Bienvenid@ a Actecom, especialistas en informática y telecomunicaciones. En breve podréis estar al dia de nuestras novedades, nuestras ofertas y nuestros servicios.</p>
+    <div className="container">
+      <div className='Navbar'>
+     <Logo/>
+                </div>
     </div>
   );
 }
---
+++ actecom_front/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo=()=>{
+
+    return(
+
+    <div className='logo'>
+    <h1>Actecom</h1>
+      <h3>Informática y telecomunicaciones</h3>
+        </div>
+)}
+
+export default Logo;
